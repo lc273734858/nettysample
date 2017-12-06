d7f096fa24897f34406bef39f98914044cee1fdf
刘吉国 <liujg@5173.com>
2017-12-06 17:54:10 +0800

    post code first

---
+++ mqapi/src/main/java/fww/mqapi/MqapiApplication.java
@@ -0,0 +1,12 @@
+package fww.mqapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqapiApplication.class, args);
+	}
+}
---
+++ nettyserver/src/main/java/nettysample/nettyserver/App.java
@@ -0,0 +1,14 @@
+package nettysample.nettyserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    } 
+}
